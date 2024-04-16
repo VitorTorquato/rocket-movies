fb056dfa14bcf0336f8acbefd93de4c10465925d
Vitor <torquatovitor68@gmail.com>
2024-04-16 13:20:50 +0200

    backend integration start

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,14 +1,40 @@
-
+import { useState } from 'react'
 import { FiMail , FiLock, FiUser , FiArrowLeft} from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Container ,Form, Background} from "./styles.js"
 
+import { api } from '../../service/api.js'
 
 import { Input } from '../../components/input'
 import { Button } from '../../components/button'
 
  
 export function SignIn(){
+
+    const [name , setName] = useState("");
+    const [email , setEmail] = useState("");
+    const [password , setPassword] = useState("");
+    	
+
+    const navigate = useNavigate()
+
+    function handleSignUp(){
+        if(!name || !email || !password){
+            return alert('Preencha todos os campos')
+        }
+
+        api.post("/users" , { name , email , password})
+        .then(() => {
+            alert("Usuário cadastrado com sucesso");
+            navigate("/login")
+        }).catch(error => {
+            if(error.response){
+                alert(error.response.data.message)
+            }else{
+                alert('Não foi possivel cadastrar o usuário')
+            }
+        })
+    }
 
     return(
         <Container>
@@ -22,20 +48,23 @@ export function SignIn(){
                    placeholder="Nome"
                    type="text"
                    icon={FiUser} 
+                   onChange={e => setName(e.target.value)}
                 />
                 <Input
                    placeholder="E-Mail"
                    type="email"
                    icon={FiMail} 
+                   onChange={e => setEmail(e.target.value)}
                 />
                 <Input
                    placeholder="Senha"
                    type="password"
                    icon={FiLock} 
+                   onChange={e => setPassword(e.target.value)}
                 />
 
                 <Button
-                    title="Entrar"
+                    title="Entrar" onClick={handleSignUp}
                 />
 
                 <Link to="/login"> <FiArrowLeft/>  Voltar para o login</Link>
